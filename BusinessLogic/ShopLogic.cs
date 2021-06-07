22ceb4f889b225ce219ed33595800b8c17bc8d4f
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-06-07 14:27:42 +0300

    shoplogic added

---
+++ BusinessLogic/ShopLogic.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleShop.Models;
+using ConsoleShop.Models.Base;
+
+namespace ConsoleShop.BusinessLogic
+{
+    public class ShopLogic
+    {
+        public static List<Item> ShopInventory = new List<Item>()
+        {
+            new Book()
+            {
+                Name = "The Unofficial Holy Bible for Minecrafters. A children's guide to the Old & New Testament",
+                Quantity = 7,
+                PriceDecimal = 25.49M
+            },
+            new Book()
+            {
+                Name = "The Action Bible. God's redemptive story.",
+                Quantity = 1,
+                PriceDecimal = 30.00M
+            },
+            new Sweet()
+            {
+                Name = "A little tin of sheep poo",
+                Quantity = 20,
+                PriceDecimal = 9.99M
+            },
+            new Sweet()
+            {
+                Name = "Fudge",
+                Quantity = 40,
+                PriceDecimal = 3.75M
+            },
+            new Sweet()
+            {
+                Name = "Brittle",
+                Quantity = 80,
+                PriceDecimal = 5.95M
+            },
+            new Cup()
+            {
+                Name = "Jacob's poison",
+                Quantity = 13,
+                PriceDecimal = 6.66M
+            }
+        };
+        public static string Sell(object itemSold, int quantity, Customer customer)
+        {
+            string saleOperation = "";
+            switch (itemSold)
+            {
+                case "book":
+                    var book = ShopInventory.FirstOrDefault(i => i is Book);
+                    if (book.Quantity < quantity)
+                    {
+                        saleOperation = $"Insufficient stock levels of {book}. Please offer the customer lower quantity or a substitute product.";
+                        break;
+                    } else if (book.Quantity * book.PriceDecimal > customer.AccountBalance)
+                    {
+                        saleOperation =
+                            "Insufficient customer account balance. Please offer a lower quantity or a substitute product.";
+                        break;
+                    }
+                    else
+                    {
+                        book.Quantity -= quantity;
+                        customer.AccountBalance -= quantity * book.PriceDecimal;
+                        break;
+                    } 
+                case "cup":
+                    var cup = ShopInventory.FirstOrDefault(i => i is Cup);
+                    if (cup.Quantity < quantity)
+                    {
+                        saleOperation =
+                            $"Insufficient stock levels of {cup}. Please offer the customer lower quantity or a substitute product.";
+                        break;
+                    }
+                    else if (cup.Quantity * cup.PriceDecimal > customer.AccountBalance)
+                    {
+                        saleOperation =
+                            "Insufficient customer account balance. Please offer a lower quantity or a substitute product.";
+                        break;
+                    }
+                    else
+                    {
+                        cup.Quantity -= quantity;
+                        customer.AccountBalance -= quantity * cup.PriceDecimal;
+                        break;
+                    }
+                case "sweet":
+                    var sweet = ShopInventory.FirstOrDefault(i => i is Sweet);
+                    if (sweet.Quantity < quantity)
+                    {
+                        saleOperation = $"Insufficient stock levels of {sweet}. Please offer the customer lower quantity or a substitute product.";
+                        break;
+                    } else if (sweet.Quantity * sweet.PriceDecimal > customer.AccountBalance)
+                    {
+                        saleOperation =
+                            "Insufficient customer account balance. Please offer a lower quantity or a substitute product.";
+                        break;
+                    }
+                    else
+                    {
+                        sweet.Quantity -= quantity;
+                        customer.AccountBalance -= quantity * sweet.PriceDecimal;
+                        break;
+                    }
+            }
+
+            return saleOperation;
+
+        }
+
+        public static void CustomerBalanceTopUp(Customer customer, decimal amount)
+        {
+            customer.AccountBalance += amount;
+            Console.WriteLine($"The new balance is: £{customer.AccountBalance}");
+        }
+        public static void AddItem(string itemName, int quantity)
+        {
+            switch (itemName)
+            {
+                case "book":
+                    var book = ShopInventory.FirstOrDefault(i => i is Book);
+                    book.Quantity += quantity;
+                    break;
+                case "cup":
+                    var cup = ShopInventory.FirstOrDefault(i => i is Cup);
+                    cup.Quantity += quantity;
+                    break;
+                case "sweet":
+                    var sweet = ShopInventory.First(i => i is Sweet);
+                    sweet.Quantity += quantity;
+                    break;
+            }
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using ConsoleShop.Models;
 using ConsoleShop.Models.Base;
 using System.Linq;
+using ConsoleShop.BusinessLogic;
 
 namespace ConsoleShop
 {
@@ -10,78 +11,8 @@ namespace ConsoleShop
     {
         static void Main(string[] args)
         {
-            Shop shoppe = new Shop();
+            ShopUI shoppe = new ShopUI();
             shoppe.Open();
-
-            List<PhoneItem> phones = new List<PhoneItem>();
-            phones.Add(new PhoneItem()
-            {
-                Name = "iPhone",
-                PriceDecimal = 1000
-            });
-
-            phones.Add(new PhoneItem()
-            {
-                Name = "Nokia",
-                PriceDecimal = 200
-            });
-
-            phones.Add(new PhoneItem()
-            {
-                Name = "Blackberry",
-                PriceDecimal = 400
-            });
-
-            phones.Add(new PhoneItem()
-            {
-                Name = "Samsung",
-                PriceDecimal = 800
-            });
-
-            var phoneLessThan600 = phones.Where(p => p.PriceDecimal < 600).Select(p => p.Name);
-            Console.WriteLine(phoneLessThan600);
-
-            //var user = new Customer("Jokūbas", 250);
-            var command = "";
-            while (command != "Exit")
-            {
-                Console.WriteLine("Enter your command, please.");
-                try
-                {
-                    command = Console.ReadLine();
-                    var number = Decimal.Parse(command);
-                    Console.WriteLine($"Number supplied was {number}.");
-                    ;
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"Something went wrong:  {e.Message} Please try again");
-                }
-            }
-
-
-            //var shop = new Shop();
-
-            //var tvItem = new TvItem()
-            //{
-            //    Name = "Phillips Brilliance",
-            //    PriceDecimal = 349.99M
-            //};
-
-            //var smartWatchItem = new SmartWatchItem()
-            //{
-            //    Name = "PRO TREK",
-            //    PriceDecimal = 219.99M,
-            //    Brand = "Casio"
-            //};
-
-            //Console.WriteLine(tvItem.ToDescriptionString());
-            //Console.WriteLine(smartWatchItem.ToDescriptionString());
-            //shop.ListItems();
-            //shop.Buy("ItemName", 50);
-            //shop.LoadItems("ItemName", 50);
-
-            //Console.ReadLine();
         }
     }
 }
